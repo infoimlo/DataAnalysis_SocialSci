6262f5b8f136a79764f1e67dbcd3f5501d1306f1
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-15 21:23:49 -0400

    another correct answer

--- HWK_6/Hwk6code.R
+++ HWK_6/Hwk6code.R
@@ -21,7 +21,7 @@ thetain <- (theta>=ll ZZZ theta<=ul)
 mean(thetain)
 
 
-#Generating 1000 samples of size n - Option A 
+#Generating 1000 samples of size n - Option A  -NOT IT
 # Result -> mean(thetain) = 1
 theta <- 5
 #sample size
@@ -40,7 +40,7 @@ mean(thetain)
 
 
 # Option B -> mean = 0.04; 2nd: 0.2; 3rd: 0.16; 4th: 0.12; 5th: 0.2; 6th: 0.08
-theta <- 5
+theta <- 5        # MAYBE IT???   - NOT IT CUS THE PROBABLITY DOESN'T END UP CLOSE TO 0.9
 #sample size
 n <- 25
 #Generating 1000 samples of size n - original
@@ -56,7 +56,7 @@ thetain <- (theta>=ll & theta<=ul)
 mean(thetain)
 
 
-# Option C -> mean = 0.912; 0.919; 0.903
+# Option C -> mean = 0.912; 0.919; 0.903    # MAYBE IT?!?  - CORRECT BECAUSE THE PROBABABILTY ROUNDS CLOSEST TO P(...) = 0.9
 #Real value of theta
 theta <- 5
 #sample size
@@ -76,25 +76,8 @@ mean(thetain)
 
 
 
-# Option C -> mean = 0.912; 0.919; 0.903
-#Real value of theta
-theta <- 5
-#sample size
-n <- 25
 
-#Generating 1000 samples of size n - original
-simul <- 1000
-sample <- matrix(runif(1000*n, max = theta), 
-                 nrow = n)
-
-thetahat <- (n+1)/n*apply(sample, 2 , max)
-
-ll <- thetahat/(0.95^(1/n)*(n+1)/n)
-ul <- thetahat/(0.05^(1/n)*(n+1)/n)
-thetain <- (theta>=ll &  theta<=ul)
-mean(thetain)
-
-# Option D -> mean(thetain)
+# Option D -> mean(thetain)   # DEF NOT IT!!
 #Real value of theta
 theta <- 5
 #sample size
@@ -105,7 +88,7 @@ simul <- 1000
 sample <- matrix(runif(1000*n, max = n), 
                  nrow = n)
 
-thetahat <- (n+1)/n*apply(sample, YY2Y, max)
+thetahat <- (n+1)/n*apply(sample, 2, max)
 
 ll <- thetahat/(0.95^(1/n)*(n+1)/n)
 ul <- thetahat/(0.05^(1/n)*(n+1)/n)
@@ -114,7 +97,7 @@ mean(thetain)
 
 
 
-
+#OPTION E     - NOT IT! 
 #Real value of theta
 theta <- 5
 #sample size
