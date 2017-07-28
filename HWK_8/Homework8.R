7e09bd04a7e3624fdeb68fbc18cc280041b66aa2
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-28 15:40:49 -0400

    sucessfully completed questions 1 to 4

--- HWK_8/Homework8.R
+++ HWK_8/Homework8.R
@@ -3,6 +3,13 @@
 
 rm(list = ls())
 getwd() #get working directory - figure out what directory working out of 
-setwd("C:/Users/loret/Desktop/DataSciencePrep/Data_Analysis_for_Social_Scientists/DataAnalysis_SocialSci/HWK_7")
+setwd("C:/Users/loret/Desktop/DataSciencePrep/Data_Analysis_for_Social_Scientists/DataAnalysis_SocialSci/HWK_8")
 
-read.csv('teachers_final.csv')
+mydata <- read.csv('nlsw88.csv', header = TRUE, row.names = NULL )
+
+# Estimate equation by OLS
+results = lm(formula = lwage ~ yrs_school, data = mydata)
+summary(results)
+
+# print 90% confidence interval for beta1 
+confint(results, 'yrs_school', level = 0.90)
