660304037f77f6a500bef8e7dc857ec93a7318a8
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-28 16:11:08 -0400

    completed question 9

--- HWK_8/Homework8.R
+++ HWK_8/Homework8.R
@@ -6,6 +6,7 @@ getwd() #get working directory - figure out what directory working out of
 setwd("C:/Users/loret/Desktop/DataSciencePrep/Data_Analysis_for_Social_Scientists/DataAnalysis_SocialSci/HWK_8")
 
 mydata <- read.csv('nlsw88.csv', header = TRUE, row.names = NULL )
+mydata
 
 # Estimate equation by OLS
 results = lm(formula = lwage ~ yrs_school, data = mydata)
@@ -13,3 +14,11 @@ summary(results)
 
 # print 90% confidence interval for beta1 
 confint(results, 'yrs_school', level = 0.90)
+
+# caluculate residuals
+res <- residuals(results)
+res
+
+# Estimate liner model using OLS for when log(wagei) = beta not + beta1*black + epsiloni
+rrace = lm(formula = lwage ~ black, data = mydata)
+summary(rrace)
