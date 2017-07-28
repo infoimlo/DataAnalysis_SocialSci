319eafca0609d96e6d509131f79de8ce8d9965fc
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-07-27 23:29:18 -0400

    set up for homework 8

---
+++ HWK_8/Homework8.R
@@ -0,0 +1,8 @@
+#-------------------------------------------------
+# Homework 8 
+
+rm(list = ls())
+getwd() #get working directory - figure out what directory working out of 
+setwd("C:/Users/loret/Desktop/DataSciencePrep/Data_Analysis_for_Social_Scientists/DataAnalysis_SocialSci/HWK_7")
+
+read.csv('teachers_final.csv')
